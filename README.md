be590b020cbdd399ae644d4d62d424adeccb4031
Junobee25 <wnsdhqo@naver.com>
2024-02-08 13:57:53 +0900

    #1 - 상호작용 알림 서비스 Spring Boot 프로젝트 세팅 및 Docker Compose 설정

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # alarm-service-msa
+
 [MSA] 상호작용 알림 서비스 도메인
---
+++ src/test/java/com/hanghae/alarmservice/AlarmServiceMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.alarmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmServiceMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
