2d5847f102af37831cb39192ed1b2b7c39861c7e
Junobee25 <wnsdhqo@naver.com>
2024-02-10 11:35:57 +0900

    #3 - FeignClient 통신을 위한 세팅

--- src/main/java/com/hanghae/alarmservice/AlarmServiceApplication.java
+++ src/main/java/com/hanghae/alarmservice/AlarmServiceApplication.java
@@ -3,19 +3,17 @@ package com.hanghae.alarmservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
+@EnableJpaAuditing
 public class AlarmServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AlarmServiceApplication.class, args);
     }
 
-    @Bean
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
-    }
 }
